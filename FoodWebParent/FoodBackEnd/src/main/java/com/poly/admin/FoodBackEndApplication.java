0c9198ea3f26600fd29c20a1ad9845419f88b4e5
Admin <khoahoangyahoo@gmail.com>
2022-06-11 13:22:01 +0700

    create project architecture

---
+++ FoodWebParent/FoodBackEnd/src/main/java/com/poly/admin/FoodBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.poly.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBackEndApplication.class, args);
+	}
+
+}
---
+++ FoodWebParent/FoodFrontEnd/src/main/java/com/poly/site/FoodFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.poly.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodFrontEndApplication.class, args);
+	}
+
+}
